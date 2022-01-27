a18357564928114ca0c80a885436e94fc4985e64
gt <jack139@gmail.com>
2022-01-27 16:32:44 +0800

    prepare data

---
+++ prepare_data.py
@@ -0,0 +1,48 @@
+from retro_pytorch.retrieval import text_folder_to_chunks_
+from retro_pytorch.retrieval import chunks_to_precalculated_knn_
+
+# mock data constants
+CHUNK_SIZE = 64
+NUM_NEIGHBORS = 2
+
+stats = text_folder_to_chunks_(
+    folder = './test_data',
+    glob = '**/*.txt',
+    chunks_memmap_path = './test_data/train.chunks.dat',
+    seqs_memmap_path = './test_data/train.seq.dat',
+    doc_ids_memmap_path = './test_data/train.doc_ids.dat',  # document ids are needed for filtering out neighbors belonging to same document appropriately during computation of nearest neighbors
+    chunk_size = CHUNK_SIZE,
+    seq_len = 2048,
+    max_chunks = 1_000_000,
+    max_seqs = 100_000
+)
+
+NUM_CHUNKS, NUM_DOCS, NUM_SEQS = stats['chunks'], stats['docs'], stats['seqs']
+
+print('NUM_CHUNKS = %d\tNUM_DOCS = %d\tNUM_SEQS = %d'%(NUM_CHUNKS, NUM_DOCS, NUM_SEQS))
+
+chunks_to_precalculated_knn_(
+    num_chunks = NUM_CHUNKS,
+    chunk_size = CHUNK_SIZE,
+    chunk_memmap_path = './test_data/train.chunks.dat',    # path to main chunks dataset
+    doc_ids_memmap_path = './test_data/train.doc_ids.dat', # path to document ids created by text_folder_to_chunks_, used for filtering out neighbors that belong to the same document
+    num_nearest_neighbors = NUM_NEIGHBORS,                   # number of nearest neighbors you'd like to use
+    num_extra_neighbors = 10                     # fetch 10 extra neighbors, in the case that fetched neighbors are frequently from same document (filtered out)
+)
+
+
+'''
+from retro_pytorch.retrieval import chunks_to_index_and_embed
+
+index, embeddings = chunks_to_index_and_embed(
+    num_chunks = 1000,
+    chunk_size = 64,
+    chunk_memmap_path = './test_data/train.chunks.dat'
+)
+
+query_vector = embeddings[:1]                   # use first embedding as query
+_, indices = index.search(query_vector, k = 2)  # fetch 2 neighbors, first indices should be self
+
+neighbor_embeddings = embeddings[indices]       # (1, 2, 768)
+'''
+
--- train.py
+++ train.py
@@ -1,23 +1,58 @@
 import torch
-from retro_pytorch import RETRO
-from transformers import AutoTokenizer, EncoderDecoderModel
+from torch.utils.data import DataLoader
+from retro_pytorch import RETRO, RETRODataset
+import numpy as np
 
-column = "whole_func_string"
-encoder_name = "microsoft/codebert-base"
-decoder_name = "gpt2"
-encoder_tokenizer = AutoTokenizer.from_pretrained(encoder_name)
-decoder_tokenizer = AutoTokenizer.from_pretrained(decoder_name)
-decoder_tokenizer.pad_token = decoder_tokenizer.eos_token
 
-# 中文的句子embedding考虑 "sentence-transformers/paraphrase-multilingual-mpnet-base-v2"
-retro_ds = RetroDataset(
-    "code_search_net",
-    "flax-sentence-embeddings/st-codesearch-distilroberta-base",
-    encoder_tokenizer,
-    decoder_tokenizer,
-    dataset_config="python",
-    column=column,
-    batch_size=4,
-    k=2,
-    n_perc=2
+
+# mock data constants
+NUM_CHUNKS = 66 # news1.txt NUM_CHUNKS = 66 NUM_DOCS = 1    NUM_SEQS = 3
+NUM_SEQS = 3
+CHUNK_SIZE = 64
+NUM_NEIGHBORS = 2
+
+# instantiate dataset class
+# which constructs the sequence and neighbors from memmapped chunk and neighbor information
+
+train_ds = RETRODataset(
+    num_sequences = NUM_SEQS,
+    num_chunks = NUM_CHUNKS,
+    num_neighbors = NUM_NEIGHBORS,
+    chunk_size = CHUNK_SIZE,
+    seq_len = 2048,
+    chunk_memmap_path = './test_data/train.chunks.dat',
+    chunk_nn_memmap_path = './test_data/train.chunks.knn.dat',
+    seq_memmap_path = './test_data/train.seq.dat'
 )
+
+train_dl = iter(DataLoader(train_ds, batch_size = 2))
+
+# one forwards and backwards
+
+retro = RETRO(
+    max_seq_len = 2048,                      # max sequence length
+    enc_dim = 896,                           # encoder model dimension
+    enc_depth = 3,                           # encoder depth
+    dec_dim = 768,                           # decoder model dimensions
+    dec_depth = 12,                          # decoder depth
+    dec_cross_attn_layers = (1, 3, 6, 9),    # decoder cross attention layers (with causal chunk cross attention)
+    heads = 8,                               # attention heads
+    dim_head = 64,                           # dimension per head
+    dec_attn_dropout = 0.25,                 # decoder attention dropout
+    dec_ff_dropout = 0.25                    # decoder feedforward dropout
+)#.cuda()
+
+'''
+seq, retrieved = map(lambda t: t.cuda(), next(train_dl))
+
+# seq       - (2, 2049)         - 1 extra token since split by seq[:, :-1], seq[:, 1:]
+# retrieved - (2, 32, 2, 128)   - 128 since chunk + continuation, each 64 tokens
+
+loss = retro(
+    seq,
+    retrieved,
+    return_loss = True
+)
+
+loss.backward()
+'''
